1dde37bcf3b5ac8d424da0dba12210c40f4fdc05
Jackson <jackson.engstrom@succurri.com>
2014-08-15 11:37:00 -0700

    reverted and added a println

---
+++ gittest/src/de/vogella/git/first/GitTest.java
@@ -0,0 +1,9 @@
+package de.vogella.git.first;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		System.out.println("Git if fun");
+		System.out.println("Woohoo its working (hopefully)");
+	}//main(String[])
+} // GitTest
